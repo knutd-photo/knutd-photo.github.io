6595d22437039c88d9c444a4cd93031768a8c55b
knutd-photo <halliana.rose@gmail.com>
2016-12-22 19:28:36 +0200

    sliders

--- scripts/SliderScript.js
+++ scripts/SliderScript.js
@@ -1,25 +1,56 @@
 ﻿$(document).ready(function () {
     $(".slider").each(function () { 
         var obj = $(this);
-        $(obj).append("<div class='nav'></div>");
+        $(obj).append("<div id='sliderNav' class='nav'></div>");
         $(obj).find("li").each(function () {
             $(obj).find(".nav").append("<span rel='" + $(this).index() + "'></span>");
             $(this).addClass("slider" + $(this).index());
         });
         $(obj).find("span").first().addClass("on");
+
+        _counter = 0;
+
+        setInterval(function () {
+            var sl = document.getElementsByClassName('slider');
+            var li = $(sl).find('li');
+
+            if (_counter >= li.length) {
+                _counter = 0;
+            }
+
+            autoSlid(_counter);
+
+            _counter++;
+        }, 4000)
     });
 });
-function sliderJS(obj, sl) {
-    var ul = $(sl).find("ul");
-    var bl = $(sl).find("li.slider" + obj);
-    var step = $(bl).width();
-    $(ul).animate({ marginLeft: "-" + step * obj }, 500);
-}
+
 $(document).on("click", ".slider .nav span", function () { 
     var sl = $(this).closest(".slider");
     $(sl).find("span").removeClass("on"); 
     $(this).addClass("on");
     var obj = $(this).attr("rel");
-    sliderJS(obj, sl);
+    _counter = obj;
+    autoSlid(obj);
     return false;
 });
+
+function autoSlid(count) {
+    var sl = document.getElementsByClassName('slider');
+    var ul = $(sl).find('ul');
+    var li = $(ul).find('li');
+    var step = $(li[0]).width();
+
+    var navDiv = document.getElementById('sliderNav');
+    $(navDiv).find('span').removeClass('on')
+    var navSpans = $(navDiv).find('span');
+
+    for (var i = 0; i < navSpans.length; i++) {
+        var val = navSpans[i].getAttribute('rel');
+        if (val == count) {
+            $(navSpans[i]).addClass('on');
+        }
+    }
+
+    $(ul).animate({ marginLeft: "-" + step * count }, 700);
+}
